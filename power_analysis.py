c983d6ac326ab2fc1c891d47c2430c061b9f01c4
julio0029 <86091929+julio0029@users.noreply.github.com>
2022-11-02 21:13:40 +1300

    Update power_analysis.py

--- power_analysis.py
+++ power_analysis.py
@@ -2,7 +2,7 @@ from statsmodels.stats.power import TTestIndPower
 # parameters for power analysis
 effect = 0.8
 alpha = 0.05
-power = 0.8e
+power = 0.8
 # perform power analysis
 analysis = TTestIndPower()
 result = analysis.solve_power(effect, power=power, nobs1=None, ratio=1.0, alpha=alpha)
